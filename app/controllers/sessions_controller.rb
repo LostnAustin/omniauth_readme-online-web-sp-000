c955bad57d1f8fa6e9440feb5d44c604605d8cce
Chad Lengefeld <chadlengefeld@gmail.com>
2021-03-27 04:35:20 +0000

    Automatically backed up by Learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,6 +4,7 @@ class SessionsController < ApplicationController
       u.name = auth['info']['name']
       u.email = auth['info']['email']
       u.image = auth['info']['image']
+      binding.pry
     end
 
     session[:user_id] = @user.id
@@ -12,7 +13,7 @@ class SessionsController < ApplicationController
   end
 
   private
- 
+
   def auth
     request.env['omniauth.auth']
   end
