7fb47357d55ee26c7c431811f47822f7947ab5d4
Chad Lengefeld <chadlengefeld@gmail.com>
2021-03-27 04:35:50 +0000

    Automatically backed up by Learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,7 +4,7 @@ class SessionsController < ApplicationController
       u.name = auth['info']['name']
       u.email = auth['info']['email']
       u.image = auth['info']['image']
-    
+
     end
 
   binding.pry
